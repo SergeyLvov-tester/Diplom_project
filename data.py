b2151205f6a936a90b9700949a8bf5a63182e0d6
konstantinovvn <v.konstantinov@haulmont.com>
2024-07-12 15:10:21 +0400

    Initial commit

---
+++ data.py
@@ -0,0 +1,17 @@
+headers = {
+    "Content-Type": "application/json"
+}
+
+order_body = {
+    "firstName": "Иван",
+    "lastName": "Иванов",
+    "address": "г. Москва, Красная площадь, Кремль, 1",
+    "metroStation": 4,
+    "phone": "+7 999 999 99 99",
+    "rentTime": 5,
+    "deliveryDate": "2024-14-07",
+    "comment": "Быстрее, метнись кабанчиком, пивас стынет!!!",
+    "color": [
+        "BLACK"
+    ]
+}
